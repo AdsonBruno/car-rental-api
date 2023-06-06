c0994c5d8a911ec9721e5e796d9ea1d79fc3355e
AdsonBruno <adsonbruno2@gmail.com>
2023-06-06 18:33:04 -0300

    test: ensure SignUpController returns 400 if no password is provide

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,4 +31,19 @@ describe('SignUp Controller', () => {
     expect(httResponse.statusCode).toBe(400);
     expect(httResponse.body).toEqual(new MissingParamsError('email'));
   });
+
+  test('Shoul return 400 if no password is provide', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        passwordConfirmation: 'any_password',
+        profileImage: 'any_image.png',
+      },
+    };
+    const httResponse = sut.handle(httpRequest);
+    expect(httResponse.statusCode).toBe(400);
+    expect(httResponse.body).toEqual(new MissingParamsError('password'));
+  });
 });
