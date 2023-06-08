e99c6a01ffc30f27890ef61ef0ff308197438fac
AdsonBruno <adsonbruno2@gmail.com>
2023-06-08 01:55:36 -0300

    feat: ensure SignUpController returns 400 if an email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,8 +2,16 @@ import { MissingParamsError } from '../errors/missing-params-error';
 import { HttpResponse, HttpRequest } from '../protocols/http';
 import { badRequest, created } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
+import { EmailValidator } from '../protocols/email-validator';
+import { InvalidParamError } from '../errors/invalid-param-erros';
 
 export class SignUpController implements Controller {
+  private readonly emailValidator: EmailValidator;
+
+  constructor(emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredField = ['name', 'email', 'password', 'passwordConfirmation'];
     for (const field of requiredField) {
@@ -12,12 +20,16 @@ export class SignUpController implements Controller {
       }
     }
 
+    const isValid = this.emailValidator.isValid(httpRequest.body.email);
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'));
+    }
     const { profileImage } = httpRequest.body;
 
     if (profileImage) {
       return created('User created sucessfully', profileImage);
+    } else {
+      return created('User created sucessfully');
     }
-
-    return created('User created sucessfully');
   }
 }
