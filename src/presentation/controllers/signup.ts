773f206fbd352c33cf73af385d352d3714a69116
AdsonBruno <adsonbruno2@gmail.com>
2023-06-08 13:27:30 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,10 +1,9 @@
 import { MissingParamsError } from '../errors/missing-params-error';
 import { HttpResponse, HttpRequest } from '../protocols/http';
-import { badRequest, created } from '../helpers/http-helper';
+import { badRequest, created, serverError } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
 import { EmailValidator } from '../protocols/email-validator';
 import { InvalidParamError } from '../errors/invalid-param-erros';
-import { ServerError } from '../errors/server-error';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -39,10 +38,7 @@ export class SignUpController implements Controller {
         return created('User created sucessfully');
       }
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError(),
-      };
+      return serverError();
     }
   }
 }
--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,9 +1,5 @@
+import { ServerError } from '../errors/server-error';
 import { HttpResponse } from '../protocols/http';
-
-export const badRequest = (error: Error): HttpResponse => ({
-  statusCode: 400,
-  body: error,
-});
 
 export const created = (
   message: string,
@@ -24,3 +20,13 @@ export const created = (
     body: { message },
   };
 };
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const serverError = (): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(),
+});
