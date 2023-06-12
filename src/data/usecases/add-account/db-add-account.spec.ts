3f3a81e88b1cae1b5e503692818ecd08fcc9274c
AdsonBruno <adsonbruno2@gmail.com>
2023-06-12 10:16:09 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -96,4 +96,21 @@ describe('DbAddAccount UseCase', () => {
       profileImage: 'valid_image.png',
     });
   });
+
+  test('Should throw if DbAddAccount throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error()))
+      );
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+      profileImage: 'valid_image.png',
+    };
+    const promise = sut.add(accountData);
+    await expect(promise).rejects.toThrow();
+  });
 });
