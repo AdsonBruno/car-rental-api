0e6b8041233911fd08750b0c3f2d2b497d7bd8f3
AdsonBruno <adsonbruno2@gmail.com>
2023-06-15 17:45:31 -0300

    refactor: move prismaclient methods to a helper

---
+++ src/infra/criptography/db/prisma/account-repository/helpers/prisma-helper.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@prisma/client';
+
+export const PrismaHelper = {
+  client: null as PrismaClient,
+
+  async connect(): Promise<void> {
+    this.client = new PrismaClient();
+    await this.client.$connect();
+  },
+
+  async disconnect(): Promise<void> {
+    await this.client.$disconnect();
+  },
+};
