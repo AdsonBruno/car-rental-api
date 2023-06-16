60c9529319964fe98ae4a62e0bbc2514b3428111
AdsonBruno <adsonbruno2@gmail.com>
2023-06-16 19:03:02 -0300

    test: ensure SignUp route returns an account success

---
+++ src/main/routes/signup-route.test.ts
@@ -0,0 +1,17 @@
+import app from '../config/app';
+import request from 'supertest';
+
+describe('SignUp Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Adson',
+        email: 'adsonbruno2@gmail.com',
+        password: '123',
+        passwordConfirmation: '123',
+        profileImage: 'any_image.png',
+      })
+      .expect(200);
+  });
+});
