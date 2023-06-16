b6ffb51f38897afc7712221fdc3aee2da2c46c09
AdsonBruno <adsonbruno2@gmail.com>
2023-06-15 23:36:06 -0300

    test: ensure AccountPrismaRepository returns an account on success

---
+++ src/infra/db/prisma/account-repository/account.spec.ts
@@ -0,0 +1,28 @@
+import { AccountPrismaRepository } from './account';
+import { PrismaHelper } from './helpers/prisma-helper';
+
+describe('Account Postgres Repository', () => {
+  beforeAll(async () => {
+    await PrismaHelper.connect();
+  });
+
+  afterAll(async () => {
+    await PrismaHelper.disconnect();
+  });
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountPrismaRepository();
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      profileImage: 'any_image.png',
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe('any_name');
+    expect(account.email).toBe('any_email@mail.com');
+    expect(account.password).toBe('any_password');
+    expect(account.profileImage).toBe('any_image.png');
+  });
+});
