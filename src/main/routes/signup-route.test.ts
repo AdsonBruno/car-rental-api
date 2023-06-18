08a5df48338bf35f024f819339de534d8eb74ad4
AdsonBruno <adsonbruno2@gmail.com>
2023-06-18 10:41:33 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-route.test.ts
+++ src/main/routes/signup-route.test.ts
@@ -1,7 +1,17 @@
+import { AccountPrismaRepository } from '../../infra/db/prisma/account-repository/account';
+import { PrismaHelper } from '../../infra/db/prisma/helpers/prisma-helper';
 import app from '../config/app';
 import request from 'supertest';
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    await PrismaHelper.connect();
+  });
+
+  afterAll(async () => {
+    await PrismaHelper.disconnect();
+  });
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
