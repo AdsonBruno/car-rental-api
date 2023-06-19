f5a701f3153042c31ef98d6a5b577ea658c528fc
AdsonBruno <adsonbruno2@gmail.com>
2023-06-19 09:35:58 -0300

    feat: add ExpressRouteAdapter

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,5 @@ export default {
   postgresUrl:
     process.env.DATABASE_URL ||
     'postgresql://postgres:postgres@localhost:5432/rental_car_api_test?schema=public',
-  port: process.env.PORT || 5432,
+  port: process.env.PORT || 5050,
 };
